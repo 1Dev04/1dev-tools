88c79f4f625e1b63c81d0453d1354174d5df9073
Devdummyy <krittiphon.yoon@gmail.com>
2024-03-07 08:22:48 -0800

    V2 three commit

--- src/component/View.js
+++ src/component/View.js
@@ -4,11 +4,11 @@ const View = () =>{
     return (
         <div className="view"> 
             <div className="Page-view">
-                <span id="page-count"> </span>
+                <span id="page-count"> 0 </span>
                 <h5 className="bi bi-emoji-heart-eyes-fill"> PageViews</h5>
             </div>
             <div className="Visit-view">
-                <span id="view-count"></span>
+                <span id="view-count"> 0 </span>
                 <h5 className="bi bi-eye-fill"> Visits</h5>
             </div>
         </div>
