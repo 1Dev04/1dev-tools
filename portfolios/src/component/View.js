ce015c5bc947a905c6b8d071f9d5da83502041be
Devdummyy <krittiphon.yoon@gmail.com>
2024-03-09 23:34:14 -0800

    V2.1 Two commit

--- portfolios/src/component/View.js
+++ portfolios/src/component/View.js
@@ -1,45 +1,40 @@
+import React, { useState, useEffect } from 'react';
 import '../customCSS/App.css';
 
+const View = () => {
+  const [pageViews, setPageViews] = useState(0);
+  const [visitCount, setVisitCount] = useState(0);
 
-const View = () =>{
+  useEffect(() => {
+    const fetchData = async () => {
+      const type = sessionStorage.getItem('visit') === null ? 'type=visit-pageview' : 'type=pageview';
 
-    const pageView = document.getElementById('.Page-view .page-count');
-    const visitCount = document.getElementById('.Visit-view .view-count');
+      try {
+        const response = await fetch(`http://172.20.10.4:3000?${type}`);
+        const data = await response.json();
 
-    if(sessionStorage.getItem('visit') === null){
-        updateCounter('type=visit-pageview');
-    } else {
-        updateCounter('type=pageview');
-    }
+        setPageViews(data.pageviews);
+        setVisitCount(data.visits);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-    sessionStorage.getItem('visit', 'x');
+    fetchData();
+  }, []); // Empty dependency array ensures the effect runs once after the initial render
 
-
-    function updateCounter(type){
-        fetch('http://172.20.10.4:3000?' + type)
-        .then(res => res.json() )
-        .then(data => {
-            pageView.textContent = data.pageviews;
-            visitCount.textContent = data.visits;
-        })
-    }
-
-
-
-    return (
-        <div className="view"> 
-
-            <div className="Page-view">
-                <span id="page-count"></span>
-                <h5 className="bi bi-emoji-heart-eyes-fill"> PageViews</h5>
-            </div>
-            <div className="Visit-view">
-                <span id="view-count"></span>
-                <h5 className="bi bi-eye-fill"> Visits</h5>
-            </div>
-
-        </div>
-    ); 
-}
+  return (
+    <div className="view">
+      <div className="Page-view">
+        <span id="page-count">{pageViews}</span>
+        <h5 className="bi bi-emoji-heart-eyes-fill"> PageViews</h5>
+      </div>
+      <div className="Visit-view">
+        <span id="view-count">{visitCount}</span>
+        <h5 className="bi bi-eye-fill"> Visits</h5>
+      </div>
+    </div>
+  );
+};
 
 export default View;
