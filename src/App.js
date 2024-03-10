ab5a24bd369ffc54e484acd1d84a6ce6ef477d8a
Devdummyy <krittiphon.yoon@gmail.com>
2024-03-10 00:41:22 -0800

    V2.2 first commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './customCSS/style-switcher.css';
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Landing from './page/LandingPages.js';
+import Landing from '../src/page/LandingPages.js';
 import FontEnd from './page/FontEnd.js';
 import BackEnd from './page/BackEnd.js';
 
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './customCSS/index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals.js';
+import reportWebVitals from '../src/reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
