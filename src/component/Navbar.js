865ab6b8bc6d964939866fae109e29233f5fe0f7
Devdummyy <devdummy.h@gmail.com>
2024-09-02 07:39:31 -0700

    V4.6

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import "../customCSS/input.css";
 import "../customCSS/App.css";
-import "../customCSS/Navbar.css";
+import "../customCSS/navbar.css";
 
 
 const Navbar = () => {
