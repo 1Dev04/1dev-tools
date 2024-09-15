c908594d49d0d7ae60bb03ffcce5ce339f61e66a
Devdummyy <devdummy.h@gmail.com>
2024-09-15 06:08:30 -0700

    V4.7

--- src/component/PageView.js
+++ src/component/PageView.js
@@ -5,9 +5,9 @@ import '../customCSS/View.css';
 import '../customCSS/style-switcher.css';
 
 const ViewCount = () => {
-  const [viewCount, setViewCountState] = useState(''); 
+  const [viewCount, setViewCountState] = useState('');
   const [likeCount, setLikeCountState] = useState('');
- 
+
 
   useEffect(() => {
     // ฟังก์ชันดึงและอัพเดตจำนวนการเข้าชม
@@ -16,18 +16,18 @@ const ViewCount = () => {
       const snapshot = await get(viewRef);
 
       if (snapshot.exists()) {
-        const currentCount = snapshot.val(); 
+        const currentCount = snapshot.val();
         const updatedCount = currentCount + 1;
-        set(viewRef, updatedCount); 
-        setViewCountState(updatedCount); 
+        set(viewRef, updatedCount);
+        setViewCountState(updatedCount);
       } else {
-        set(viewRef, 1); 
-        setViewCountState(1); 
+        set(viewRef, 1);
+        setViewCountState(1);
       }
     };
 
     fetchAndUpdateViewCount();
-    
+
     const fetchAndUpdateLikeCount = async () => {
       const likeRef = ref(database, "likes");
       const snapshot = await get(likeRef);
@@ -49,11 +49,15 @@ const ViewCount = () => {
     if (snapshot.exists()) {
       const currentCount = snapshot.val();
       const updatedCount = currentCount + 1;
-      set(likeRef, updatedCount); 
-      setLikeCountState(updatedCount); 
+      const textAlert = document.querySelector(".body-view .text-Al")
+      const iconAlert = document.querySelector(".alert .close-btn")
+      set(likeRef, updatedCount);
+      setLikeCountState(updatedCount);
+      textAlert.classList.add("hidden")
+      iconAlert.classList.add("hidden")
     }
 
-  
+
 
   };
 
@@ -64,8 +68,13 @@ const ViewCount = () => {
       </h4>
       <h4 className="name1 like-page" onClick={handleLikeClick}>
         <button><i className='bx bxs-heart color-icon'></i> </button>
-        <strong> {likeCount}</strong>
       </h4>
+      <strong className="text-heart"> {likeCount}</strong>
+      <div class="alert">
+          <span className="close-btn"><i class='bx bxs-up-arrow-alt'></i></span>
+      </div>
+      <br></br>
+      <div className="text-Al">Click on the <strong>Heart</strong>.</div>
     </div>
   );
 };
