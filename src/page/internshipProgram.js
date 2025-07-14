489b1938b694e074d498f85976ed026ac72afc99
Devdummyy <devdummy.h@gmail.com>
2025-07-14 10:13:05 +0700

    v7

--- src/page/internshipProgram.js
+++ src/page/internshipProgram.js
@@ -23,7 +23,7 @@ function InternshipProgram() {
       icon3: <i style={{ color: "#00758F", fontSize: 15 }} className="bx bxs-data"></i>,
       icon4: <i style={{ color: "#fca121" }} className="bx bxl-gitlab"></i>,
       icon5: <i style={{ color: "#7952B3" }} className="bx bxl-bootstrap"></i>,
-      iconVSColor: "bg-gray-800", 
+      iconVSColor: "bg-gray-800",
       dotColor: "bg-green-500",
     },
     {
@@ -103,79 +103,79 @@ function InternshipProgram() {
   ];
 
   return (
-    <div className="frontend">
+    <div className="internship">
       <div className=" footer foot-color  p-2 fixed-top box ">
         <div className="container-fluid">
           <div className="navbar-brand rounded  fs-1">Internship Project</div>
         </div>
       </div>
 
-      <section className=" container-fluid  py-4 " id="showtime">
+      <section className=" container-fluid  py-4 ">
         <div className="row flex justify-center m-5 p-2">
-          <section id="faq" className="hero min-h-screen py-12">
-  <div className="max-w-xl mx-auto py-40 center">
-    <ul className="relative border-l-2 border-gray-200">
-      {items.map((item, i) => (
-        <li key={i} className="mb-28 ml-7"> 
-          <span
-            className={`absolute -left-7 flex items-center justify-center w-12 h-12 rounded-full ${item.dotColor}`}  /* เพิ่มขนาดวงกลมไอคอน */
-            style={{ fontSize: '2rem' }} 
-          >
-            {item.icon}
-          </span>
+          <section id="faq" className="hero min-h-screen py-4 w-100 h-100">
+            <div className="max-w-xl mx-auto py-40 center">
+              <ul className="relative border-l-2 border-gray-200">
+                {items.map((item, i) => (
+                  <li key={i} className="mb-28 ml-7">
+                    <span
+                      className={`absolute -left-7 flex items-center justify-center w-12 h-12 rounded-full ${item.dotColor}`}  /* เพิ่มขนาดวงกลมไอคอน */
+                      style={{ fontSize: '2rem' }}
+                    >
+                      {item.icon}
+                    </span>
 
-          <h3 className="text-xl font-semibold">{item.Month}</h3>
-          <p className="text-lg font-normal text-gray-400">
-            {item.description}
-          </p>
-          <p className="text-lg font-normal text-gray-400">
-            {item.descriptions}
-          </p>
-          <hr className="my-4" /> 
-          <p className="text-lg font-normal text-gray-400">
-            {item.description1}
-          </p>
-          <p className="text-lg font-normal text-gray-400">
-            {item.description1s}
-          </p>
-          <br/>
-          <div className="absolute right-0 flex gap-4">
-            <span
-              className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`} 
-              style={{ fontSize: '1.5rem' }}
-            >
-              {item.icon1}
-            </span>
-            <span
-              className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`}
-              style={{ fontSize: '1.5rem' }}
-            >
-              {item.icon2}
-            </span>
-            <span
-              className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`}
-              style={{ fontSize: '1.5rem' }}
-            >
-              {item.icon3}
-            </span>
-            <span
-              className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`}
-              style={{ fontSize: '1.5rem' }}
-            >
-              {item.icon4}
-            </span>
-               <span
-              className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`}
-              style={{ fontSize: '1.5rem' }}
-            >
-              {item.icon5}
-            </span>
-          </div>
-        </li>
-      ))}
-    </ul>
-  </div>
-</section>
+                    <h3 className="text-xl font-semibold">{item.Month}</h3>
+                    <p className="text-lg font-normal text-gray-400">
+                      {item.description}
+                    </p>
+                    <p className="text-lg font-normal text-gray-400">
+                      {item.descriptions}
+                    </p>
+                    <hr className="my-4" />
+                    <p className="text-lg font-normal text-gray-400">
+                      {item.description1}
+                    </p>
+                    <p className="text-lg font-normal text-gray-400">
+                      {item.description1s}
+                    </p>
+                    <br />
+                    <div className="absolute right-0 flex gap-4">
+                      <span
+                        className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`}
+                        style={{ fontSize: '1.5rem' }}
+                      >
+                        {item.icon1}
+                      </span>
+                      <span
+                        className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`}
+                        style={{ fontSize: '1.5rem' }}
+                      >
+                        {item.icon2}
+                      </span>
+                      <span
+                        className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`}
+                        style={{ fontSize: '1.5rem' }}
+                      >
+                        {item.icon3}
+                      </span>
+                      <span
+                        className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`}
+                        style={{ fontSize: '1.5rem' }}
+                      >
+                        {item.icon4}
+                      </span>
+                      <span
+                        className={`w-12 h-12 rounded-full flex items-center justify-center ${item.iconVSColor}`}
+                        style={{ fontSize: '1.5rem' }}
+                      >
+                        {item.icon5}
+                      </span>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </section>
 
         </div>
       </section>
