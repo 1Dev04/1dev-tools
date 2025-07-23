ae1a3247a6a64ce1d954e275d39d99f621d67d96
Devdummyy <devdummy.h@gmail.com>
2025-07-23 16:23:55 +0700

    v7

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,11 +1,9 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import "../customCSS/input.css";
 import "../customCSS/App.css";
 import "../customCSS/navbar.css";
 
-
 const Navbar = () => {
-
   useEffect(() => {
     const menuIcon = document.querySelector("#menu-icon");
     const navbar = document.querySelector(".navbar");
@@ -18,75 +16,85 @@ const Navbar = () => {
     };
 
     const handleScroll = () => {
-      sections.forEach (sec => {
-        let top = window.scrollY;
+      let top = window.scrollY;
+      sections.forEach((sec) => {
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
         let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height){
-            navLinks.forEach(links => {
-                links.classList.remove("active");
-                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
-            });
-        };
-        navbar.classList.remove("active");
-    });
+        if (top >= offset && top < offset + height) {
+          navLinks.forEach((link) => {
+            link.classList.remove("active");
+          });
 
-    let header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 100);
-    menuIcon.classList.remove("bx-x");
-   
-    navbar.classList.remove("active");
-    
+          const currentLink = document.querySelector(
+            `header nav a[href*="#${id}"]`
+          );
+          if (currentLink) {
+            currentLink.classList.add("active");
+          }
+
+
+          if (window.location.hash !== `#${id}`) {
+            window.history.replaceState(null, null, `#${id}`);
+          }
+        }
+        navbar.classList.remove("active");
+      });
+
+      const header = document.querySelector("header");
+      header.classList.toggle("sticky", window.scrollY > 700);
+
+      menuIcon.classList.remove("bx-x");
+      navbar.classList.remove("active");
     };
 
     menuIcon.addEventListener("click", handleMenuClick);
     window.addEventListener("scroll", handleScroll);
 
     return () => {
-      
       menuIcon.removeEventListener("click", handleMenuClick);
       window.removeEventListener("scroll", handleScroll);
     };
-  }, []); 
+  }, []);
 
   return (
     <nav className="fixed-top">
       <header className="header">
-        <div className="logo">
+        <div className="logo">PORTFOLIO</div>
 
-          PORTFOLIO
-          
-        </div>
-        
         <i className="bx bx-menu" id="menu-icon"></i>
-       
+
         <nav className="navbar">
           <a href="#home" className="active scoll-S1">
             Home
           </a>
-          
-          <a href="#about" className="scoll-S2">About</a>
-          
-          <a href="#certificate" className="scoll-S3">Certificate</a>
-          
-          <a href="#skills" className="scoll-S4">Skills</a>
-          
-          <a href="#projects" className="scoll-S5">Projects</a>
-          
-           <a href="#activitys" className="scoll-S6">Activitys</a>
-          
+
+          <a href="#about" className="scoll-S2">
+            About
+          </a>
+
+          <a href="#certificate" className="scoll-S3">
+            Certificate
+          </a>
+
+          <a href="#skills" className="scoll-S4">
+            Skills
+          </a>
+
+          <a href="#projects" className="scoll-S5">
+            Projects
+          </a>
+
+          <a href="#activitys" className="scoll-S6">
+            Activitys
+          </a>
+
           {/* <a href="#contact" className="scoll-S6">Contact</a>  */}
-          
-          
-          
         </nav>
       </header>
-      
     </nav>
   );
-  
 };
 
 export default Navbar;
