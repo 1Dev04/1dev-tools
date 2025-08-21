1b6dace3ea8dd1f2ab47c7738c06db981040dd10
Devdummyy <devdummy.h@gmail.com>
2025-08-21 12:01:24 +0700

    update response

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './customCSS/style/iphone_XR/theme.css';
 import './customCSS/style/iphone14_pro_max/theme.css';
 import './customCSS/style/iphone13_12_pro/theme.css';
 import './customCSS/style/pixel_7/theme.css';
-import './customCSS/style/Sum_Gal_S8_S20/theme.css';
+import './customCSS/style/sum_gal_S8_S20/theme.css';
 import './customCSS/style/ipad_mini/theme.css';
 import './customCSS/style/ipad_air/theme.css';
 import './customCSS/style/ipad_pro/theme.css';
@@ -18,7 +18,9 @@ import './customCSS/style/surface_pro_7/theme.css';
 import './customCSS/style/surface_duo/theme.css';
 import './customCSS/style/galaxy_z_f_5/theme.css';
 import './customCSS/style/azuz_zenbook_fold/theme.css';
-
+import './customCSS/style/sum_galaxy_a51_71/theme.css';
+import './customCSS/style/nest_hub/theme.css';
+import './customCSS/style/nest_hub_max/theme.css';
 
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
